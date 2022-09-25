ab2140e7c0f7f2a2a99fb5a58139f58f17415d69
Matheus Sena <msena2103@gmail.com>
2022-09-25 16:01:04 -0300

    Update 1-array.php

--- 2-avancando/1-array.php
+++ 2-avancando/1-array.php
@@ -17,6 +17,8 @@ $modelos[0] = 'Senna';
 $modelos[1] = 'F430';
 $modelos[2] = 'Hurucan';
 
+$filmes[] = 'Vingadores: Ultimato'; //adiciona no próximo índice NUMÉRICO disponível
+
 // count() -> função que retorna o tamanho do array
 
 echo count($modelos); // irá retornar 3
@@ -28,5 +30,9 @@ $pessoa1 = [
     'Idade' => 24,
     'CPF' => '123.456.789-10'
 ];
+
+/*OBS: as chaves de arrays associativos devem ser inteiros ou strings, caso contrário serão convertidos,
+* segundo a tabela de conversões do PHP -> https://www.php.net/manual/pt_BR/types.comparisons.php
+*/
 
 ?>
