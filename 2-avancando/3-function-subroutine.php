2ee2a1ae4b62edf1931efa6e6954e2872a7d272b
Matheus Sena <msena2103@gmail.com>
2022-09-25 20:32:58 -0300

    Update 3-function-subroutine.php

--- 2-avancando/3-function-subroutine.php
+++ 2-avancando/3-function-subroutine.php
@@ -7,17 +7,46 @@
 */
 
 // subroutine
-
-function exibeMensagem(string $mensagem) : void // aqui específicamos o tipo de retorno
+                // --- \/ aqui específicamos o tipo e se há parâmetros para a function
+function exibeMensagem(string $mensagem) : void 
 {
     echo $mensagem . PHP_EOL;
 } 
 
 // function
 
-function somaNumeros(int $numero1, int $numero2) : int
+function somaNumeros(int $numero1, int $numero2) : int // <- aqui específicamos o tipo de retorno
 {
     return $numero1+$numero2;
 }
+
+/*
+* OBS: Diferentemente de subrotinas, uma função representa o valor calculado por ela depois de sua execução.
+*/
+
+function subtraiNumeros(int $numero1, int $numero2) : int
+{
+    return $numero1-$numero2;
+}
+$resultado = subtraiNumeros(2, 2);
+
+// SEÇÃO EXTRA ---------------------------------------------------------------------------*
+
+/*
+* Ao chamar uma função, informamos os parâmetros de acordo com a ordem indicada pela própria função,
+* mas em 2020 o PHP lançou uma RFC que permite a partir do PHP 8, inserir os parâmetros nomeados,
+* não sendo necessário informá-los em ordem -> https://wiki.php.net/rfc/named_params
+*/
+
+// função exemplo
+
+function divideNumeros(int $numero1, int $numero2) : float
+{
+    return $numero1 / $numero2 . PHP_EOL;
+}
+
+// utilizando parâmetros nomeados para evitar a ordem
+
+echo 'Resultado: ' . divideNumeros(numero2: 2, numero1: 4);
 
 ?>
