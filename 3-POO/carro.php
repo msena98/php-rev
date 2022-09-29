1ab92315fbee61071e2a8e7099e440b005365064
Matheus Sena <msena2103@gmail.com>
2022-09-29 07:51:35 -0300

    Update carro.php

--- 3-POO/carro.php
+++ 3-POO/carro.php
@@ -1,8 +1,21 @@
 <?php
 
+/* Definindo os atributos da classe Carro de acordo com o pilar de Abstração da POO, 
+* que significa abstrair informações/funcionamentos de situações no mundo real, de uma forma
+* otimizada para o que necessitamos em nosso sistema, caso deseje ver mais a respeito acesse:
+* https://pt.wikipedia.org/wiki/Abstra%C3%A7%C3%A3o_%28ci%C3%AAncia_da_computa%C3%A7%C3%A3o%29
+*/
 class Carro 
 {
-    
+    public string $marca;
+    public string $modelo;
+    public string $motor;
+    public string $cambio;
+    public string $portas;
+    public bool $ligado;
 }
+
+
+public function
 
 ?>
