42753e61b8fd2a5aee556e0c1dcc8dbc1e23a4f8
Matheus Sena <msena2103@gmail.com>
2022-09-30 07:58:58 -0300

    Update Carro.php

--- 3-POO/carro.php
+++ 3-POO/carro.php
@@ -21,10 +21,24 @@ public function ligarCarro(): void
     $this->ligado = true; //$this se refere ao objeto que "chamou" o método
 }
 
-// Métodos com referência
-public function passarBateria(Carro $carro): void
+/* Métodos com referência:
+* Recebemos uma referência de outro objeto e realizamos alterações em seus atributos.
+*/
+public function transferirBateria(Carro $carro): void
 {
-    
+    if($this->bateria < 25) 
+    {
+        echo 'Não será possível transferir bateria, nível de carga abaixo de 25%' . PHP_EOL;
+    }
+    $this->bateria -= 10;
+    $carro->bateria += 10; // <- alterando o atributo do objeto recebido por referência
+
+    /* Talvez você estranhe a maneira como foi utilizado o bloco if acima, foi utilizada o design pattern
+    * chamado "Early Return" onde se evita uso de elses por exemplo, para manter o código mais legível e 
+    * de fácil entendimento, caso queira ver mais detalhes e exemplos sobre esse design de código, acesse:
+    *  -> https://dorianneto.com.br/boas-praticas/torne-se-um-ninja-das-funcoes-com-early-return/
+    *  -> https://www.alura.com.br/artigos/quanto-mais-simples-melhor
+    */
 }
 }
 ?>
