d7fa926cfbaeb2abb4f77a8bb15cecac66c109ce
Matheus Sena <msena2103@gmail.com>
2022-10-02 12:06:10 -0300

    Create Caminhao.php

---
+++ 3-POO/Basico/3-Caminhao.php
@@ -0,0 +1,23 @@
+<?php
+
+class Caminhao
+{
+    private float $capacidade;
+    private string $eixos;
+    private readonly string $motorista; 
+    /*         /\ aqui definimos o atributo como readonly(PHP 8+), assim só será permitido defini-lo 1x,
+    *             para saber mais acesse: https://www.youtube.com/watch?v=u5hge87rCCQ
+    */
+
+    /*
+    * Método construtor __construct():
+    * Este método é "chamado" toda vez que um objeto da classe é instanciado("criado"), através dele
+    * tornamos obrigatório o envio de informações para instanciar o objeto, ou será retornado um fatal error.
+    */
+    public function __construct(string $motorista, float $capacidade, string $eixos) {
+        $this->capacidade = $capacidade;
+        $this->eixos = $eixos;
+    }
+}
+
+?>
