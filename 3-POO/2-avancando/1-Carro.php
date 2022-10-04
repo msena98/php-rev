4bfc53a5e608e4a5c00ddbc6befc7ac61aaa4225
Matheus Sena <msena2103@gmail.com>
2022-10-03 21:40:17 -0300

    Update Carro.php

--- 3-POO/2-avancando/1-Carro.php
+++ 3-POO/2-avancando/1-Carro.php
@@ -1,18 +1,23 @@
 <?php
 
 
-class Carro extends Veiculo
+class Carro extends Veiculo // através de "extends", nós extendemos a classe Veiculo.
 {
     private bool $portaMalas;
 
+    public function __construct(string $marca, string $modelo) {
+        parent::__construct($marca, $modelo); // dessa maneira, "chamamos" o construtor da classe "pai", evitando erros.
+        $this->portaMalas = true;
+    }
+
     public function abrePortaMalas()
     {
-        if ($this->portaMalas) {
-            echo 'O porta malas já se encontra aberto.';
+        if (!$this->portaMalas) {
+            echo 'O porta malas já se encontra aberto.' . PHP_EOL;
         }
 
         $this->portaMalas = false;
-        echo 'O porta malas foi aberto.';
+        echo 'O porta malas foi aberto.' . PHP_EOL;
     }
 
 }
