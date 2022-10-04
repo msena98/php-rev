086a8e83aeb1fb94e07d4fe12dc77c253c0d0e97
Matheus Sena <msena2103@gmail.com>
2022-10-03 21:07:03 -0300

    Update Caminhao.php

--- 3-POO/2-avancando/3-Caminhao.php
+++ 3-POO/2-avancando/3-Caminhao.php
@@ -1,12 +1,13 @@
 <?php
 
-class Caminhao
+class Caminhao extends Veiculo
 {
     private readonly string $motorista; 
     private float $capacidade;
     private string $eixos;
 
     public function __construct(string $motorista, float $capacidade, string $eixos) {
+        $this->motorista = $motorista;
         $this->capacidade = $capacidade;
         $this->eixos = $eixos;
     }
