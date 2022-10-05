daef485cef68791e5c6d735b39c8c7bb1833b840
Matheus Sena <msena2103@gmail.com>
2022-10-05 07:52:39 -0300

    Update namespace

--- 3-POO/3-final/Modelo/Moto.php
+++ 3-POO/3-final/Modelo/Moto.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modelo;
+namespace Sena\Carz\Modelo;
 
 class Moto extends Veiculo
 {
--- 3-POO/3-final/Modelo/Veiculo.php
+++ 3-POO/3-final/Modelo/Veiculo.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Modelo;
+namespace Sena\Carz\Modelo;
 
 class Veiculo 
 {
