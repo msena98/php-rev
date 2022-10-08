9bbadc0f016972c9d480e9833bba2a073ebf18b7
Matheus Sena <msena2103@gmail.com>
2022-10-08 14:00:04 -0300

    Update interface Limpador.php

--- 3-POO/3-final/Limpador.php
+++ 3-POO/3-final/Limpador.php
@@ -1,5 +1,6 @@
 <?php
 
+// Interface: é um contrato onde quem assina se responsabiliza por implementar esses métodos (cumprir o contrato)
 interface Limpador
 {
     public function limpaParabrisa(int $nivelDaChuva): void;
